a2037a8ea5039121fe076b0080989edc0275a1f4
罗宜峰 <luoyifeng@gmail.com>
2018-08-27 17:08:05 +0800

    Initial commit

---
+++ config.js
@@ -0,0 +1,6 @@
+let config = {
+    mnemonic: "hidden oppose around lion adult message prosper earth october grocery twist symbol",
+    provider_url: "https://rinkeby.infura.io/v3/82a59ec1db144c82873d6a0f937e4d4a"
+};
+
+module.exports = config;
